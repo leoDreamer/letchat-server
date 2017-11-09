f74d78f345b9526ca1229853f2f90cb9f7733924
devil5263 <434330692@qq.com>
2017-11-09 16:58:16 +0800

    modify auth middleware

--- app/controller/auth.js
+++ app/controller/auth.js
@@ -14,27 +14,43 @@ const userRule = {
 };
 module.exports = app => {
     class Auth extends app.Controller {
-        async register(ctx) {
+        async register(ctx) { // 注册
             ctx.validate(userRule);
-            const user = await ctx.service.auth.register(ctx.request.body);
+
+            // 注册用户
+            const user = await ctx.model.User.create(ctx.request.body);
             ctx.assert(user, "注册失败");
 
             ctx.status = 204;
         }
-        async login(ctx) {
+
+        async login(ctx) { // 登录
             ctx.validate(userRule);
-            const user = await ctx.service.auth.login(ctx.request.body);
+
+            // 用户校验
+            const { name, passwd } = ctx.request.body;
+            const user = await this.ctx.model.User.findOne({
+                where: {
+                    name: name,
+                    passwd: passwd
+                }
+            });
             ctx.assert(user, "用户名或密码错误");
-            const token = await app.genToken(user.id, ctx.request.ip);
-            ctx.session.user = user.id;
-            ctx.session.token = token.id;
-            ctx.body = ctx.session;
+
+            // 生成token和session并存储
+            const token = await ctx.service.token.genToken(user.id, ctx.request.ip);
+            await app.redis.set(`${app.options.sessionPrefix}:${token.id}`, JSON.stringify({
+                user: user.id,
+                token: token.id
+            }));
+            ctx.cookies.set("access_token", token.id);
 
             ctx.status = 204;
         }
 
-        async logout(ctx) {
-            ctx.session = null;
+        async logout(ctx) { // 登出
+            await app.redis.del(`${app.options.sessionPrefix}:${ctx.state.auth.token}`);
+            ctx.cookies.set("access_token", null);
 
             ctx.status = 204;
         }
--- app/extend/content.js
+++
@@ -1,9 +0,0 @@
-const moment = require("moment");
-
-const genToken = async function (userId, ip) {
-    const expire = new Date(moment().add(1, "days"));
-    const token = await this.model.Token.create({ expire, ip, user: userId, valid: true });
-    return token;
-};
-
-module.exports = { genToken };
