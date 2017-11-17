cb0a15d065bedbd351fb8ed443e17c77dfd1d847
devil5263 <434330692@qq.com>
2017-11-17 15:47:51 +0800

    add vue-router config/demo done

--- resource/app.vue
+++ resource/app.vue
@@ -1,5 +1,12 @@
 <template>
-    <span>{{msg}} </span>
+    <div>
+        <p>This is App Component Msg: {{msg}}</p>
+        <h5>Router-link :</h5>
+        <p><router-link to="/pagea">Go A</router-link></p>
+        <p><router-link to="/pageb">Go B</router-link></p>
+        <h5>Router-view :</h5>
+        <router-view></router-view>
+    </div>
 </template>
 <script>
     export default {
@@ -12,8 +19,4 @@
     }
 </script>
 <style rel="stylesheet/scss" lang="scss">
-    $test-color: red;
-    span {
-        color: $test-color
-    }
 </style>
--- resource/index.js
+++ resource/index.js
@@ -3,11 +3,26 @@
  */
 import Vue from "vue";
 import App from "./app.vue";
+import Router from "vue-router";
+
+import PageA from "./pages/pagea";
+import PageB from "./pages/pageb";
+
+const routes = [
+    { path: "/pagea", component: PageA },
+    { path: "/pageb", component: PageB }
+];
+
+const router = new Router({
+    routes
+});
+Vue.use(Router);
 
 Vue.config.debug = true;
 
 new Vue({
     el: "#app",
     template: "<App/>",
-    components: { App }
+    components: { App },
+    router
 });
